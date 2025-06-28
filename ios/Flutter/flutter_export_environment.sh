3a8de0a7629f45ec9f1b322f15b6024ee7710aed
Arce Bracamonte, Sebastian Rodrigo <sa2019062986@virtual.upt.pe>
2025-06-27 19:06:18 -0500

    v2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Win\Desktop\moviles2Proyecto"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\HP\Desktop\SDASDASD\SM2_ExamenUnidad3"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
